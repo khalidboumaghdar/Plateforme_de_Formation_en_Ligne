f389fd8635ad315668e69ebe88990a9b28b3d7b9
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-07-12 20:43:31 +0000

    j'ai créé certificate service

---
+++ certificate-service/src/test/java/com/enaa/certificate_service/CertificateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaa.certificate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
