6dd9f2f1d2fccb409db8adf00d7c5e9ced7314ad
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-07-12 20:42:02 +0000

    j'ai créé meeting service

---
+++ meeting-service/src/test/java/com/enaa/meeting_service/MeetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaa.meeting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
